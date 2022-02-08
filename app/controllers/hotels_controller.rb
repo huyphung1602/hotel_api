0abc649b28a51e4fb72835f015a3527c27b3b4cb
huyphung1602 <huy.phung.sw@gmail.com>
2022-02-08 20:34:58 +0700

    Use fetcher to get all data

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,16 +1,6 @@
-require 'net/http'
-require 'uri'
-
 class HotelsController < ApplicationController
   def index
-    response = get_basic_uri
-    render json: response
-  end
-
-  private
-
-  def get_basic_uri
-    uri = URI('http://www.mocky.io/v2/5ebbea002e000054009f3ffc')
-    Net::HTTP.get(uri)
+    hotels = ::DataFetcherService.fetch_data('hotels')
+    render json: hotels
   end
 end
