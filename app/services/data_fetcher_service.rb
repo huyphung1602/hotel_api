5fa4490a3fc0f9608ce1211e4bb698976874aef6
huyphung1602 <huy.phung.sw@gmail.com>
2022-02-08 20:34:44 +0700

    Add DataSources service, DataFetcherService

---
+++ app/services/data_fetcher_service.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+require 'uri'
+
+class DataFetcherService
+  class << self
+    def fetch_data(source_type)
+      sources = DataSources.get_source(source_type)
+      sources.reduce([]) do |arr, source|
+        uri = URI(source)
+        responses = JSON.parse(Net::HTTP.get(uri))
+
+        if responses.is_a?(Array)
+          arr += responses
+        else
+          raise "Invalid response data: #{responses.inspect}"
+        end
+
+        arr
+      end
+    end
+  end
+end
---
+++ app/services/data_sources.rb
@@ -0,0 +1,18 @@
+class DataSources
+  HOTEL_SOURCE_URLS = [
+    'http://www.mocky.io/v2/5ebbea102e000029009f3fff',
+    'http://www.mocky.io/v2/5ebbea002e000054009f3ffc',
+    'http://www.mocky.io/v2/5ebbea1f2e00002b009f4000',
+  ].freeze
+
+  class << self
+    def get_source(source_type)
+      case source_type.to_sym
+      when :hotels
+        HOTEL_SOURCE_URLS
+      else
+        raise 'Unsupported source type'
+      end
+    end
+  end
+end
