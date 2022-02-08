0d627174e729b4ce6dd89b8e6e0cace30eaea6f8
huyphung1602 <huy.phung.sw@gmail.com>
2022-02-08 19:47:57 +0700

    Update basic json fetcher (for testing)

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,4 +1,16 @@
+require 'net/http'
+require 'uri'
+
 class HotelsController < ApplicationController
   def index
+    response = get_basic_uri
+    render json: response
+  end
+
+  private
+
+  def get_basic_uri
+    uri = URI('http://www.mocky.io/v2/5ebbea002e000054009f3ffc')
+    Net::HTTP.get(uri)
   end
 end
