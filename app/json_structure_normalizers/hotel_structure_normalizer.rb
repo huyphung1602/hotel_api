930805e289f6711f04cf66f732a08b67a2bffac2
huyphung1602 <huy.phung.sw@gmail.com>
2022-02-09 01:46:54 +0700

    Encapsulate the merge logic inside the normalizer. Add code to merge adhoc columns (description)

--- app/json_structure_normalizers/hotel_structure_normalizer.rb
+++ app/json_structure_normalizers/hotel_structure_normalizer.rb
@@ -13,9 +13,16 @@ class HotelStructureNormalizer
     address: 'address',
     city: 'city',
     country: 'country',
+    description: 'description',
+    details: 'description',
+    info: 'description',
     amenities: 'amenities',
     images: 'images',
     booking_conditions: 'booking_conditions',
+  }.freeze
+
+  ADHOC_COLUMNS_MERGE = {
+    'description': true,
   }.freeze
 
   class << self
@@ -25,6 +32,38 @@ class HotelStructureNormalizer
 
     def get_id(json_data)
       json_data['id'] || json_data['hotel_id'] || json_data['Id']
+    end
+
+    def get_hotel_id(data)
+      get_id(json_data)
+    end
+
+    def get_destination_id(json_data)
+      json_data['destination_id'] || json_data['destination']
+    end
+
+    def merge!(merging_row, raw_row)
+      raw_row.each do |key, value|
+        column_name = COLUMN_ALIASES_MAPPING[key.underscore.to_sym]
+
+        next if column_name.nil?
+
+        if !merging_row[column_name].present?
+          if ADHOC_COLUMNS_MERGE[column_name].present?
+            send("merge_#{column_name}", merging_row[column_name], value)
+          else
+            merging_row[column_name] = value
+          end
+        end
+      end
+
+      merging_row
+    end
+
+    # Adhoc codes for description
+    def merge_description(merging_row_description, raw_row_description)
+      longer_description = merging_row_description.size > raw_row_description.size ? merging_row_description : raw_row_description
+      longer_description
     end
   end
 end
--- app/services/data_merging_service.rb
+++ app/services/data_merging_service.rb
@@ -2,23 +2,12 @@ class DataMergingService
   class << self
     def merge_data_structure(normalizer_class_name, raw_rows)
       normalizer = normalizer_class_name.constantize
-      columns_aliases_mapping = normalizer.get_column_aliases_mapping
 
       raw_rows.reduce({}) do |hash, row|
-        current_row_id = normalizer.get_id(row)
-        current_row = hash[current_row_id].present? ? hash[current_row_id] : {}
-
-        row.each do |key, value|
-          column_name = columns_aliases_mapping[key.underscore.to_sym]
-
-          next if column_name.nil?
-
-          if !current_row[column_name].present?
-            current_row[column_name] = value
-          end
-        end
-
-        hash[current_row_id] = current_row
+        merging_row_id = normalizer.get_id(row)
+        merging_row = hash[merging_row_id].present? ? hash[merging_row_id] : {}
+        merged_row = normalizer.merge!(merging_row, row)
+        hash[merging_row_id] = merged_row
         hash
       end
     end
