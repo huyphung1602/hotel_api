1deaaa6c279109154f40c7f15ad256bd02f68cb3
huyphung1602 <huy.phung.sw@gmail.com>
2022-02-20 07:06:32 +0700

    Create Job model

---
+++ db/migrate/20220219223744_create_jobs.rb
@@ -0,0 +1,14 @@
+class CreateJobs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :jobs do |t|
+      t.integer :source_id
+      t.string :source_type
+      t.datetime :start_time
+      t.datetime :end_time
+      t.column :status, :integer, default: 0
+      t.jsonb :logs
+
+      t.timestamps
+    end
+  end
+end
