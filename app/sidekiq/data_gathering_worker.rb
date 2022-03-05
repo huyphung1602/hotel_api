0acb70db79e25b368232c2eddba58d11c778988f
huyphung1602 <huy.phung.sw@gmail.com>
2022-03-06 03:19:09 +0700

    Update job progress inside DataGatheringWorker

--- app/sidekiq/data_gathering_worker.rb
+++ app/sidekiq/data_gathering_worker.rb
@@ -4,15 +4,19 @@ class DataGatheringWorker
   include Sidekiq::Job
 
   def perform(source_type, filter_columns, query_key, job_id)
-    # Dedup the job if it already existed
-    if Job.any_running_job_with_query_key(source_type, query_key)
-      Job.find(job_id).already_existed
-      return
-    end
+    job = Job.find(job_id)
 
+    # Dedup the job if it already existed
+    return job.already_existed if Job.any_running_job_with_query_key(source_type, query_key)
+
+    job.start
     data = ::DataGatheringService.execute(source_type, filter_columns)
     Cache.set_cache(object_type: source_type, query_key: query_key, json_data: data.to_json)
+    job.success
     data
+  rescue StandardError => e
+    job.failed
+    raise e
   end
 
   def self.may_be_async(source_type, filter_columns, query_key, job_id)
