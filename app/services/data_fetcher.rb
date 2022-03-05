ba7c2a9ffe17a0fa2ad334686b6559a033732b10
huyphung1602 <huy.phung.sw@gmail.com>
2022-03-06 03:57:28 +0700

    Update fetcher to work normally when one or more sources failed to fetch

--- app/services/data_fetcher.rb
+++ app/services/data_fetcher.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DataFetcher
   def self.execute(source_type)
     source_urls = DataSource.get_source(source_type).get_source_urls
@@ -5,13 +7,17 @@ class DataFetcher
       uri = URI(url)
       responses = JSON.parse(Net::HTTP.get(uri))
 
+      # We are fetching data from many sources, puts the error instead of raising it
       if responses.is_a?(Array)
         arr += responses
       else
-        raise DataGatheringServiceError,
-              "Invalid response data, expecting parsed response as an array: #{responses.inspect}, method: fetch_data"
+        Rails.logger.error "DataFetcher: Expecting parsed response as an array: #{responses.inspect}"
       end
 
+      arr
+
+    rescue StandardError => e
+      Rails.logger.error "#{DataFetcher}: #{e.inspect}"
       arr
     end
   end
