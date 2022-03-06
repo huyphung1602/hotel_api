fe27a2292e8d81296b5cf7e30b4a90cdae60b920
huyphung1602 <huy.phung.sw@gmail.com>
2022-03-06 22:55:08 +0700

    Fix data_source_spec

--- spec/services/data_source_spec.rb
+++ spec/services/data_source_spec.rb
@@ -3,16 +3,22 @@
 require 'rails_helper'
 
 describe DataSource do
-  describe '.get_source' do
+  describe '.from_source_type' do
+    subject { described_class.from_source_type(source_name) }
+
     context 'supported source_type' do
+      let(:source_name) { 'hotel_json' }
+
       it 'returns the correct source' do
-        expect(described_class.get_source('hotel_json')).to eq described_class::HOTEL_SOURCE_URLS
+        expect(subject).to eq HotelSource
       end
     end
 
     context 'unsupported source_type' do
+      let(:source_name) { 'wibu' }
+
       it 'raise the Unsupported Type error' do
-        expect { described_class.get_source('wibu') }.to raise_error(DataSourceError)
+        expect { subject }.to raise_error(DataSourceError)
       end
     end
   end
